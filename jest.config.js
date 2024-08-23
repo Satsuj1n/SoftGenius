8f4eb735a93cd36b8e73acaa4c57cd978f2767a4
Felipe Lima <pegasusyt1@gmail.com>
2024-08-23 10:55:45 -0300

    adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env.development" });
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  setupFilesAfterEnv: ["<rootDir>/jest.config.js"],
+});
+
+module.exports = jestConfig;
